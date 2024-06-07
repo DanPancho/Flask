8a2b44b2dcce8f423f15c6f09981e6adbf30e69b
DanPancho <daarcosa@puce.edu.ec>
2024-06-07 00:56:58 -0500

    master: deploy

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hola, mundo!'
+def hello_world():
+    return '¡Hola, Mundo!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
